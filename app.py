e262be3d21f3df31ed70bba667d21ce9397fb84b
Eduardo Imery <ejwimery@gmail.com>
2019-10-20 12:30:56 -0600

    creating a buttom

--- app.py
+++ app.py
@@ -10,5 +10,13 @@ canvas.pack()
 #creating a frame inside or gui
 frame = tk.Frame(root, bg="white")
 frame.place(relwidth=0.8 ,relheight=0.8, relx=0.1, rely=0.1)
+
+#adding buttoms
+openFile = tk.Button(
+    root, text="Open File",
+    padx=10, pady=5,
+    bg="white",fg="#263D42"
+    )
+openFile.pack()
 #to run the app and state open
 root.mainloop()
