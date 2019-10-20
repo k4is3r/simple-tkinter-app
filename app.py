5287213b7b8c701b16f2b0382725e31408a33108
Eduardo Imery <ejwimery@gmail.com>
2019-10-20 13:09:29 -0600

    adding funciton to openFile buttom

--- app.py
+++ app.py
@@ -4,6 +4,16 @@ import os
 
 
 root = tk.Tk()
+
+#Button function
+def addApp():
+    filename = filedialog.askopenfilename(
+        initialdir="/",
+        title="Select File",
+        filetypes=(("executables","*.exe"),("all files","*.*"))
+    )
+
+
 canvas = tk.Canvas(root, height=700, width=700, bg="#263D42")
 canvas.pack()
 
@@ -15,8 +25,10 @@ frame.place(relwidth=0.8 ,relheight=0.8, relx=0.1, rely=0.1)
 openFile = tk.Button(
     root, text="Open File",
     padx=10, pady=5,
-    bg="white",fg="#263D42"
+    bg="white",fg="#263D42",
+    command=addApp
     )
+
 openFile.pack()
 
 runApps = tk.Button(
